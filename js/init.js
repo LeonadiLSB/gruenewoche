a44d49ac949a0e2ad45ee9c64e82773206fe6d68
Leon Bentrup <leon@leonadi.de>
2014-05-03 17:53:06 +0200

    Built gruenewoche from commit 613b1aa on branch master

--- js/init.js
+++
@@ -1,6 +0,0 @@
-if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  $(window).stellar({
-    horizontalScrolling: false,
-    responsive: true
-  });
-}
